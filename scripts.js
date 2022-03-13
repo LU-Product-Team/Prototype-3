cf7274dd0cb069a1c00fddb94b76a29690d900bf
zdbaas <70355019+zdbaas@users.noreply.github.com>
2022-03-13 16:57:44 -0400

    Graphs added to dashboard

--- scripts.js
+++ scripts.js
@@ -9,3 +9,30 @@ $("body").on("click", "header > .fa.fa-arrow-left", function(){
 $('a[href]').each(function(){
     $(this).attr("href", "" + $(this).attr('href') + "?" + encodeURI(window.location.href) + "");
 });
+
+// Dashboard graph functions, animations, etc.
+
+numOfDisplayedGames = $('.points_animation_column').length;
+columnWidth = 100*(1/numOfDisplayedGames);
+
+var minPoints = Number.MAX_VALUE, maxPoints = Number.MIN_VALUE;
+$(".points_animation_column").each(function () {
+    var id = parseInt(this.id, 10);
+    if (id > maxPoints) {
+        maxPoints = id;
+    }
+    if (id < minPoints) {
+        minPoints = id;
+    }
+});
+
+$('.points_animation_column').each(function() {
+    $(this).css('width','' + columnWidth + '%');
+});
+setTimeout(function() {
+    $('.points_animation_column').each(function() {
+        thesePoints = $(this).attr('id');
+        percentHeight = 100*(thesePoints/maxPoints);
+        $(this).children('div:first-child').children().css('height','' + percentHeight + '%');
+    });
+},500);
